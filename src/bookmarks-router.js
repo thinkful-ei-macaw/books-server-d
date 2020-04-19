694f242017e7d610ad298eefca406ca51390a7a7
Desmond Wareham <desmond.wareham@gmail.com>
2020-04-19 17:24:07 -0400

    working on post

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -4,28 +4,29 @@ const bookmarks = require('./store')
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
 const {isWebUri} = require('valid-url')
-let logger = console
+
 bookmarksRouter
   .route('/bookmarks')
   .get((req, res) => {
     res.json(bookmarks)
   })
   .post(bodyParser, (req, res) => {
+    console.log("req.body is ", req.body);
     for (const field of ['title', 'url', 'rating']) {
       if (!req.body[field]) {
-        // logger.error(`${field} is required`)
+        //logger.error(`${field} is required`)
         return res.status(400).send(`'${field}' is required`)
       }
     }
     const { title, url, description, rating } = req.body
 
-    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
-    //   logger.error(`Invalid rating '${rating}' supplied`)
+    if (!Number.isInteger(rating) || rating <0 || rating > 5) {
+      // logger.error(`Invalid rating '${rating}' supplied`)
       return res.status(400).send(`'rating' must be a number between 0 and 5`)
     }
 
     if (!isWebUri(url)) {
-    //   logger.error(`Invalid url '${url}' supplied`)
+      //logger.error(`Invalid url '${url}' supplied`)
       return res.status(400).send(`'url' must be a valid URL`)
     }
 
